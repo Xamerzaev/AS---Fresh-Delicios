c4f69e95f7b3a6dcbd46fef868a7671a9b0bddb2
Xamerzaev <mansur.ham@mail.ru>
2022-11-12 00:03:40 +0300

    some fixes

--- core/csv_products_script.py
+++ core/csv_products_script.py
@@ -2,15 +2,17 @@ import pandas as pd
 from csv import DictReader
 from models import Products
 import os
-import django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+import django
 django.setup()
+
+from django.core.management import call_command
 
 file_path = 'core/scripts/static/products.csv'
 
 df = pd.read_csv(file_path, delimiter="\t")
-if __name__ == '__main__':
-    for _, row in DictReader(df.iterrows()):
-        product = Products(name=row['Наименование'])
-        product.save()
+
+for _, row in DictReader(df.iterrows()):
+    product = Products(name=row['Наименование'])
+    product.save()
