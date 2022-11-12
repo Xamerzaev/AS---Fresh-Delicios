099e704385a05843b44d6736961b74d0f2f58b47
Mansur <mansur.ham@mail.ru>
2022-11-12 12:23:45 +0300

    commit

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,9 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 
